4b538929b8a26ff3df4ea625a93291329ba65103
adnanfrd <adnanmadhar74@gmail.com>
2025-05-16 14:10:41 +0500

    updated code for layout

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,22 +1,22 @@
-import { Tabs } from 'expo-router';
+// import { Tabs } from 'expo-router';
 
-export default function AuthLayout() {
-  return (
-    <Tabs>
-      <Tabs.Screen
-        name="login"
-        options={{
-          title: 'Login',
-          tabBarIcon: () => null, 
-        }}
-      />
-      <Tabs.Screen
-        name="signup"
-        options={{
-          title: 'Signup',
-          tabBarIcon: () => null,
-        }}
-      />
-    </Tabs>
-  );
-}
+// export default function AuthLayout() {
+//   return (
+//     <Tabs>
+//       <Tabs.Screen
+//         name="login"
+//         options={{
+//           title: 'Login',
+//           tabBarIcon: () => null, 
+//         }}
+//       />
+//       <Tabs.Screen
+//         name="signup"
+//         options={{
+//           title: 'Signup',
+//           tabBarIcon: () => null,
+//         }}
+//       />
+//     </Tabs>
+//   );
+// }
