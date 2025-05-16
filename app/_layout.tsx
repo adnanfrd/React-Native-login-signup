504c69936830a825f96b8ad9ca304cd240d15b4a
adnanfrd <adnanmadhar74@gmail.com>
2025-05-16 13:27:12 +0500

    added view

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,18 @@
+import { useRouter } from "expo-router";
 import { Button, Image, Text, View } from "react-native";
 
 export default function RootLayout() {
+
+  const router = useRouter();
   return (
     <View>
       <Image
       source={require('../assets/images/splash-icon.png')}
       />
       <Text>LoginSignup App</Text>
-      <Button title="Get Started"/>
+      <View>
+        <Button title="Get Started"/>
+      </View>
     </View>
   );
 }
